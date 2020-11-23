7fa3b2443cc1e602553c196e3d41157fbd1fb1bf
Sweet4ever <oskpil@gmail.com>
2020-11-23 16:18:50 +0100

    1/2 tests working

---
+++ src/test/java/ContactTest.java
@@ -0,0 +1,15 @@
+//package se.iths;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ContactTest {
+
+    @Test
+    void newContact(){
+        Contact contact = new Contact("Oskar", "Pilborg", "oskar.pilborg@mail.se");
+        assertEquals(contact.getFirstName(), "Oskar");
+    }
+    
+}
