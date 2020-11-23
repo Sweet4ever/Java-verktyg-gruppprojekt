b06424a0ad4c79925505a1f19ff628e485a5a13f
John-Rotte <john.roth_95@live.com>
2020-11-23 15:49:25 +0100

    Added terminal class with basic functions

---
+++ src/main/terminal/Terminal.java
@@ -0,0 +1,19 @@
+package main.terminal;
+
+public class Terminal {
+    //Add print menu metod och meny metod
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+    private boolean Print_menu(){
+        System.out.println("Press 0: Command list\n" +
+                            "Press 1: ");
+    }
+    private boolean Welcoming_Message(){
+        System.out.println("Welcome to Group 4's contact storage tool");
+    }
+}
