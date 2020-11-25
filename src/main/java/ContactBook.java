64728283ab4a6c0e1784b854048194a89c87a884
CooBoo84 <kennie_svensson@hotmail.com>
2020-11-25 10:20:10 +0100

    Updated with arraylist

--- src/main/java/ContactBook.java
+++ src/main/java/ContactBook.java
@@ -1,18 +1,29 @@
 import java.util.ArrayList;
 
 public class ContactBook {
+    private ArrayList<Contact> myContacts;
 
-    private int findContact (Contact contact){
-        return this.myContacts.indexOf(contact);
+    public ContactBook(){
+        this.myContacts = new ArrayList<Contact>();
     }
-    @overload
-    private int findContact(String contactName){
+
+    private int findContact(String contactFirstName){
         for(int i = 0; i < this.myContacts.size(); i++){
             Contact contact = this.myContacts.get(i);
-            if(Contact.getContact().equals(contactName)) {
+            if(contact.getFirstName().equals(contactFirstName)) {
                 return i;
             }
         }
         return -1;
+    }
+
+    public  void listContacts(){
+        System.out.println("Kontaktlista: ");
+        for(int i = 0; i < this.myContacts.size(); i++){
+            System.out.println((i + 1) + ".	Förnamn: " +
+                    this.myContacts.get(i).getFirstName() + "\n\t Efternamn: " +
+                    this.myContacts.get(i).getLastName() + "\n\t E-mail: " +
+                    this.myContacts.get(i).getEmail());
+        }
     }
 }
