c2a1a65894c300f5fb487052983fe72c9e84a438
CooBoo84 <kennie_svensson@hotmail.com>
2020-12-02 09:57:45 +0100

    Added try-catch on methods

--- src/main/java/se/iths/contact/ContactBook.java
+++ src/main/java/se/iths/contact/ContactBook.java
@@ -10,60 +10,79 @@ public class ContactBook {
     }
 
     public boolean addNewContact(Contact contact) {
-        if(findContact(contact.getFirstName()) >=0) {
-            System.out.println("Kontakten är redan sparad.");
+        try {
+            if (findContact(contact.getFirstName()) >= 0) {
+                System.out.println("Kontakten är redan sparad.");
+                return false;
+            }
+            myContacts.add(contact);
+            return true;
+        }
+        catch (Exception e) {
+            System.out.println("Execption addNewContact: " + e);
             return false;
         }
-        myContacts.add(contact);
-        return true;
     }
 
     private int findContact(String contactFirstName){
-        for(int i = 0; i < this.myContacts.size(); i++){
-            Contact contact = this.myContacts.get(i);
-            if(contact.getFirstName().equals(contactFirstName)) {
-                return i;
+        try {
+            for (int i = 0; i < this.myContacts.size(); i++) {
+                Contact contact = this.myContacts.get(i);
+                if (contact.getFirstName().equals(contactFirstName)) {
+                    return i;
+                }
             }
+            return -1;
         }
-        return -1;
+        catch (Exception e) {
+            System.out.println("Execption findContact: " + e);
+            return -1;
+        }
     }
 
     public void listContacts(){
-        System.out.println("Contactlist: ");
+        try {
+            System.out.println("Contactlist: ");
 
-        for(int i = 0; i < this.myContacts.size(); i++){
-            System.out.println((i + 1) + ".	 First name: " +
-                    this.myContacts.get(i).getFirstName() + "\n\t Last name:  " +
-                    this.myContacts.get(i).getLastName() + "\n\t E-mail:     " +
-                    this.myContacts.get(i).getEmail());
+            for (int i = 0; i < this.myContacts.size(); i++) {
+                System.out.println((i + 1) + ".	 First name: " +
+                        this.myContacts.get(i).getFirstName() + "\n\t Last name:  " +
+                        this.myContacts.get(i).getLastName() + "\n\t E-mail:     " +
+                        this.myContacts.get(i).getEmail());
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Execption listContact: " + e);
         }
     }
     public void printContact(String firstName){
-        int indexPosition = findContact(firstName);
-        System.out.println("\t First name: " +
-                this.myContacts.get(indexPosition).getFirstName() + "\n\t Last name:  " +
-                this.myContacts.get(indexPosition).getLastName() + "\n\t E-mail:     " +
-                this.myContacts.get(indexPosition).getEmail());
-
+        try {
+            int indexPosition = findContact(firstName);
+            System.out.println("\t First name: " +
+                    this.myContacts.get(indexPosition).getFirstName() + "\n\t Last name:  " +
+                    this.myContacts.get(indexPosition).getLastName() + "\n\t E-mail:     " +
+                    this.myContacts.get(indexPosition).getEmail());
+        }
+        catch (Exception e) {
+            System.out.println("Execption printContact: " + e);
+        }
     }
 
     public void delete(String firstName){
-        int index = findContact(firstName);
-        if(index != -1){
-            myContacts.remove(index);
+        try {
+            int index = findContact(firstName);
+            if (index != -1) {
+                myContacts.remove(index);
+            } else {
+                System.out.println("Could not find " + firstName + " in list of contacts");
+            }
         }
-        else{
-            System.out.println("Could not find " + firstName + " in list of contacts");
+        catch (Exception e) {
+            System.out.println("Execption delete: " + e);
         }
-    }
-
-    public void addContact(String firstName, String lastName, String email){
-        Contact contact = new Contact(firstName, lastName, email);
-        myContacts.add(contact);
     }
 
     public int len(){
         return myContacts.size();
     }
-
 }
